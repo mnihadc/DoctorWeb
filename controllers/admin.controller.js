36b6465be38aefe4b179e70f445adba2ee8bc4e4
Nihad <mncnihad@gmail.com>
2024-10-28 08:19:44 +0530

    admin dashboard users displayed

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,8 +1,12 @@
-const dashboard = (req, res, next) => {
+const User = require("../models/User.model");
+
+const dashboard = async (req, res, next) => {
+  const users = await User.find({});
   res.render("admin/Dashboard", {
     title: "User Management",
     layout: "Layout/main",
     isDashboardPage: true,
+    users,
     isAdmin: req.session.user?.isAdmin,
     isAuthenticated: !!req.session.user,
   });
--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const { dashboard } = require("../controllers/admin.controller");
-const verifyToken = require("../middleware/verifyToken");
 const route = express.Router();
 
 route.get("/dashboard", dashboard);
