6a06507cbc1face9f93227a56a4d4c6490db8a9a
Nihad <mncnihad@gmail.com>
2024-10-28 09:11:40 +0530

    edit user admin dashboard

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -11,4 +11,27 @@ const dashboard = async (req, res, next) => {
     isAuthenticated: !!req.session.user,
   });
 };
-module.exports = { dashboard };
+const updateUser = async (req, res) => {
+  const id = req.params.id;
+  const { username, email } = req.body;
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      { username, email },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    // Send back the updated user data as JSON
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.error("Error updating user:", error);
+    res.status(500).json({ message: "Server error." });
+  }
+};
+
+module.exports = { dashboard, updateUser };
--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const { dashboard } = require("../controllers/admin.controller");
+const { dashboard, updateUser } = require("../controllers/admin.controller");
 const verifyToken = require("../middleware/verifyToken");
 const route = express.Router();
 
 route.get("/dashboard", verifyToken, dashboard);
+route.put("/update-user/:id", verifyToken, updateUser);
+
 module.exports = route;
