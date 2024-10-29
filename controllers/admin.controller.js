7a02a51c180f192dd121423cbb1a722f942c93d6
Nihad <mncnihad@gmail.com>
2024-10-29 17:35:42 +0530

    bcrypt remove and bcryptjs install

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,5 +1,5 @@
 const User = require("../models/User.model");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const dashboard = async (req, res, next) => {
   const users = await User.find({});
