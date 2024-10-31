6b63e2589cef56ec011744038a3836313ec3323b
Nihad <mncnihad@gmail.com>
2024-10-31 20:31:42 +0530

    creatd About page route and controller and update UI

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -41,5 +41,14 @@ const getDoctorDetails = async (req, res, next) => {
     next(error);
   }
 };
+const getAbout = (req, res, next) => {
+  res.render("partials/About", {
+    title: "About",
+    layout: "Layout/main",
+    isAboutPage: true,
+    isAdmin: req.session.user?.isAdmin,
+    isAuthenticated: !!req.session.user,
+  });
+};
 
-module.exports = { getHome, getDoctor, getDoctorDetails };
+module.exports = { getHome, getDoctor, getDoctorDetails, getAbout };
--- routes/home.route.js
+++ routes/home.route.js
@@ -3,6 +3,7 @@ const {
   getHome,
   getDoctor,
   getDoctorDetails,
+  getAbout,
 } = require("../controllers/home.controller");
 const verifyToken = require("../middleware/verifyToken");
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get("/", getHome);
 router.get("/doctor", getDoctor);
 router.get("/getdoctor/:doctorName", verifyToken, getDoctorDetails);
+router.get("/about", getAbout);
 
 module.exports = router;
